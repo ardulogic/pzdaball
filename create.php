a66da36398ca35e20768dde88f82ef4f21698e03
ardulogic <denncath@gmail.com>
2019-02-18 15:56:13 +0200

    Create, Join (No Cookies)

--- create.php
+++ create.php
@@ -1,39 +1,63 @@
 <?php
-define('STORAGE_FILE', 'files/form_input.txt');
 
-require_once 'functions/form.php';
-require_once 'functions/file.php';
+require_once 'bootstrap.php';
 
 function form_success($safe_input, $form) {
-	// TO DO
+    $team = [
+        'team_name' => $safe_input['team_name'],
+        'players' => []
+    ];
+
+    if (file_exists(STORAGE_FILE)) {
+        $teams_array = file_to_array(STORAGE_FILE);
+        $teams_array[] = $team;
+    } else {
+        $teams_array = [$team];
+    }
+
+    return array_to_file($teams_array, STORAGE_FILE);
 }
 
-function form_fail($safe_input, $form) {
-    // TO DO
+function validate_team_name($field_input, &$field, $form_input) {
+    if (file_exists(STORAGE_FILE)) {
+        $teams_array = file_to_array(STORAGE_FILE);
+
+        foreach ($teams_array as $team) {
+            if ($team['team_name'] == $field_input) {
+                $field['error_msg'] = strtr('Pz2aball team pavadinimu '
+                        . '"@team" jau egzistuoja!', [
+                    '@team' => $field_input
+                ]);
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 $form = [
     'fields' => [
         'team_name' => [
+            'label' => 'Create team',
             'type' => 'text',
             'placeholder' => 'Team name',
             'validate' => [
-                'validate_not_empty'
+                'validate_not_empty',
+                'validate_team_name'
             ]
         ],
     ],
     'buttons' => [
         'submit' => [
-            'text' => 'Create'
+            'text' => 'Create!'
         ]
     ],
     'callbacks' => [
         'success' => [
             'form_success'
         ],
-        'fail' => [
-            'form_fail'
-        ]
+        'fail' => []
     ]
 ];
 
@@ -44,6 +68,9 @@ if (!empty($_POST)) {
     $form_success = validate_form($safe_input, $form);
 
     if ($form_success) {
+        $success_msg = strtr('Team`as "@team_name" sėkmingai sukurtas!', [
+            '@team_name' => $safe_input['team_name']
+        ]);
         $show_form = false;
     }
 }
@@ -51,25 +78,21 @@ if (!empty($_POST)) {
 <html>
     <head>
         <link rel="stylesheet" href="css/style.css">
-        <title>Call Friday</title>
+        <title>PZ2ABALL | Create Team</title>
     </head>
     <body>
-        <h1>Call Friday</h1>
-		<?php if ($show_form): ?>
-			<form method="POST">
-				<?php foreach ($form['fields'] as $field_id => $field): ?>
-					<label>
-						<input type="<?php print $field['type']; ?>" 
-							   name="<?php print $field_id; ?>" 
-							   placeholder="<?php print $field['placeholder']; ?>"/>
-					</label>
-				<?php endforeach; ?>
-				<?php foreach ($form['buttons'] as $button_id => $button): ?>
-					<button name="action" value="<?php print $button_id; ?>">
-						<?php print $button['text']; ?>
-					</button>
-				<?php endforeach; ?>
-			</form>
-		<?php endif; ?>
+        <!-- Navigation -->    
+        <?php require 'objects/navigation.php'; ?>        
+
+        <!-- Content -->    
+        <h1>Create a PZ2ABALL team!</h1>
+
+        <?php if ($show_form): ?>
+            <!-- Form -->        
+            <?php require 'objects/form.php'; ?>
+        <?php else: ?>
+            <h2>Zašibys!</h2>
+            <h3><?php print $success_msg; ?></h3>
+        <?php endif; ?>
     </body>
 </html>
